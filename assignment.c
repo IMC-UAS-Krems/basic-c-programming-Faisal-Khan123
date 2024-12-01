aaf342be4eca94ab195e740c23f0b944a8b0e615
Faisal-Khan123 <153495590+Faisal-Khan123@users.noreply.github.com>
2024-12-01 14:06:51 +0100

    3rd Commit

--- assignment.c
+++ assignment.c
@@ -17,33 +17,33 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Converting arguments to integers
+    // Converting arguments from strings to integers
     int rows = atoi(argv[1]);
     int cols = atoi(argv[2]);
 
-    // Checking if arguments are positive integers
+    // Checking if arguments are positive integers for rows and columns
     if (rows <= 0 || cols <= 0) {
         printf("Incorrect usage. The parameters you provided are not positive integers\n");
-        return 1;
+        return 1; // Exiting if the arguments are invalid
     }
 
-    // Dynamically allocating memory for the matrix
+    // Allocating memory for the matrix dynamically
     int **matrix = (int **)malloc(rows * sizeof(int *));
     for (int i = 0; i < rows; i++) {
         matrix[i] = (int *)malloc(cols * sizeof(int));
     }
 
-    // Populating the matrix with random integers between 1 and 100
+    // Filling the matrix with random numbers between 1 and 100
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             matrix[i][j] = minrand + rand() % (maxrand - minrand + 1);
         }
     }
 
-    // Creating and writing the matrix to "matrix.txt"
+    // Creating and writing the matrix to the file "matrix.txt"
     FILE *file = fopen("matrix.txt", "w");
     if (file == NULL) {
-        perror("Error opening file");
+        perror("Error opening file");  // If file does not open
         for (int i = 0; i < rows; i++) {
             free(matrix[i]);
         }
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
 
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            fprintf(file, "%d", matrix[i][j]);
+            fprintf(file, "%d", matrix[i][j]); // Printing the number to the file
             if (j < cols - 1) {
                 fprintf(file, " "); // Printing a space between numbers except at the end of a line
             }
@@ -71,7 +71,7 @@ int main(int argc, char *argv[]) {
     // Printing a confirmation message
     printf("Matrix of size %d x %d was successfully created in 'matrix.txt'.\n", rows, cols);
 
-    // Freeing dynamically allocated memory
+    // Freeing up the memory allocated for the matrix
     for (int i = 0; i < rows; i++) {
         free(matrix[i]);
     }
